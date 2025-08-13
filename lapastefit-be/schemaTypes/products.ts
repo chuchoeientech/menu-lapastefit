2f13057af2bfb96e2a7a798eb0d49cb448d3b4d5
Carlos Baez <carlos@eientech.io>
2025-08-13 20:23:24 -0300

    sanity configuration

---
+++ lapastefit-be/schemaTypes/products.ts
@@ -0,0 +1,34 @@
+import {defineField, defineType} from 'sanity'
+
+export const products = defineType({
+  name: 'products',
+  title: 'Productos',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'nombre',
+      title: 'Nombre',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'descripcion',
+      title: 'Descripción',
+      type: 'text',
+    }),
+    defineField({
+      name: 'precio',
+      title: 'Precio',
+      type: 'number',
+      validation: (Rule) => Rule.min(0),
+    }),
+    defineField({
+      name: 'imagen',
+      title: 'Imagen',
+      type: 'image',
+      options: {hotspot: true},
+    }),
+  ],
+})
+
+
